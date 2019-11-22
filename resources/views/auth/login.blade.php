19561a2013368b70b8751322a12fd8f86ad8a0df
Shalom Acacio <shalom.acacio@gmail.com>
2019-11-22 01:16:14 -0300

    alterando tela login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,22 +33,22 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-8">
+                    {{-- <div class="col-8">
                         <div class="checkbox icheck">
                             <label>
                 <input type="checkbox"> Remember Me
               </label>
                         </div>
-                    </div>
+                    </div> --}}
                     <!-- /.col -->
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
 
-            <div class="social-auth-links text-center mb-3">
+            {{-- <div class="social-auth-links text-center mb-3">
                 <p>- OR -</p>
                 <a href="#" class="btn btn-block btn-primary">
           <i class="fab fa-facebook-f mr-2"></i> Sign in using Facebook
@@ -56,15 +56,15 @@
                 <a href="#" class="btn btn-block btn-danger">
           <i class="fab fa-google-plus-g mr-2"></i> Sign in using Google+
         </a>
-            </div>
+            </div> --}}
             <!-- /.social-auth-links -->
-
+{{--
             <p class="mb-1">
                 <a href="#">I forgot my password</a>
             </p>
             <p class="mb-0">
                 <a href="{{route('register')}}" class="text-center">Register a new membership</a>
-            </p>
+            </p> --}}
         </div>
         <!-- /.login-card-body -->
     </div>
