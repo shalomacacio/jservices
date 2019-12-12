9bd2b77b6ccc427c9bbd77caf1bf3e6287e62ede
Shalom Acacio <shalom.acacio@gmail.com>
2019-12-12 17:16:18 -0300

    removendo tipoUsuarioSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call([
-        TipoUsuarioSeeder::class,
         UsersTableSeeder::class,
         CategoriaServicoSeeder::class,
         StatusSolicitacaoSeeder::class,
