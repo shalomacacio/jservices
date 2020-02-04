17ca195280cd3d08a7ec767d88094c7be840264a
Shalom Acacio <shalom.acacio@gmail.com>
2020-02-03 22:49:11 -0300

    nova pagina inicial

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{!! route('dashboard') !!}" class="brand-link">
+    <a href="{!! route('comissao.minhasComissoes') !!}" class="brand-link">
     <img src="/img/logo.png" alt="Laravel Starter" class="brand-image img-circle elevation-3"
    style="opacity: .8">
 <span class="brand-text font-weight-light">JNet Serviços</span>
